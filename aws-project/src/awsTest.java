e5c14753344810321f24ec3b5e7953bab194a18c
matthewsmkim <forme3328@naver.com>
2021-12-06 12:38:27 +0900

    + 'available zones'

--- aws-project/src/awsTest.java
+++ aws-project/src/awsTest.java
@@ -8,10 +8,15 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesRequest;
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
+
+
 
 
 public class awsTest {
@@ -49,7 +54,7 @@ public class awsTest {
         init();
         Scanner menu = new Scanner(System.in);
         Scanner id_string = new Scanner(System.in);
-        int number=0;
+        
         while (true) {
             System.out.println("                                                            ");
             System.out.println("                                                            ");
@@ -66,7 +71,7 @@ public class awsTest {
             System.out.println("                                 99. quit                   ");
             System.out.println("------------------------------------------------------------");
             System.out.print("Enter an integer: ");
-            
+            int number=menu.nextInt();
 //<생략>
             switch (number) {
                 case 1:
@@ -160,9 +165,19 @@ public class awsTest {
     private static void availableZones() {
     	System.out.println("Available zones....");
     	
+    	DescribeAvailabilityZonesResult zones_response =
+    		    ec2.describeAvailabilityZones();
+
+    		for(AvailabilityZone zone : zones_response.getAvailabilityZones()) {
+    		    System.out.printf(
+    		        "[id] %s " + "[region] %s " + "[zone] %s",
+    		        zone.getZoneId(),
+    		        zone.getRegionName(),
+    		        zone.getZoneName()
+    		        );
+    		}
+    
     }
-    
-    
     
     
 }
